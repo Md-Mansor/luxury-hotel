bb9a22c7d215745b3e4d0e86f0f3f8abbfda47a2
Md-Mansor <mansor.me777@ gmail.com>
2023-11-08 01:52:01 +0600

    try to use react modal for booking room

--- src/Pages/Shared/RoomDetails/RoomDetails.jsx
+++ src/Pages/Shared/RoomDetails/RoomDetails.jsx
@@ -4,10 +4,11 @@ import { useParams } from "react-router-dom";
 
 const RoomDetails = () => {
 
+
     const [roomDetails, setRoomDetails] = useState([])
     const { id } = useParams();
     console.log(id, roomDetails);
-    const { Photo, Price, One, Two, Three, Title, Location } = roomDetails;
+    const { Photo, Price, One, Two, Description, Title, Size } = roomDetails;
 
 
     useEffect(() => {
@@ -17,17 +18,45 @@ const RoomDetails = () => {
             .then(data => {
                 setRoomDetails(data);
             })
-    }, []);
+    }, [id]);
 
     return (
-        <div>
-            <img src={Photo} alt="" />
-            <img src={One} alt="" />
-            <img src={Two} alt="" />
-            <img src={Three} alt="" />
-            <p>{Price}</p>
-            <p>{Title}</p>
-            <p>{Location}</p>
+        <div className="flex gap-10  p-12">
+            <div className="w-3/4">
+                <div className="carousel w-full">
+                    <div id="slide1" className="carousel-item relative w-full">
+                        <img src={Photo} className="w-full rounded-md h-96" />
+                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                            <a href="#slide3" className="btn btn-circle">❮</a>
+                            <a href="#slide2" className="btn btn-circle">❯</a>
+                        </div>
+                    </div>
+                    <div id="slide2" className="carousel-item relative w-full">
+                        <img src={One} className="w-full rounded-md h-96" />
+                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                            <a href="#slide1" className="btn btn-circle">❮</a>
+                            <a href="#slide3" className="btn btn-circle">❯</a>
+                        </div>
+                    </div>
+                    <div id="slide3" className="carousel-item relative w-full">
+                        <img src={Two} className="w-full rounded-md h-96" />
+                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                            <a href="#slide2" className="btn btn-circle">❮</a>
+                            <a href="#slide1" className="btn btn-circle">❯</a>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="w-1/2 py-4 flex flex-col gap-4 ">
+                    <p className="text-2xl font-bold">{Title}</p>
+                    <p className="text-xl font-semibold">{Description}</p>
+                </div>
+            </div>
+            <div className="w-1/4 border-4 border-red-300 border-dotted rounded-xl" >
+                <p>Price Per Night : $ {Price}</p>
+                <p>Room Size :{Size} ft<sup>2</sup></p>
+                <button className="btn btn-outline btn-error">Book Now</button>
+            </div>
         </div>
     );
 };
