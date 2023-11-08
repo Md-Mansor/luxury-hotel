fea6527283b5f0e23fba2eca9883981653ee1e2f
Md-Mansor <mansor.me777@ gmail.com>
2023-11-08 18:39:29 +0600

    add delete button for delete booked item

--- src/Pages/Shared/Bookings/Bookings.jsx
+++ src/Pages/Shared/Bookings/Bookings.jsx
@@ -1,21 +1,71 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../../Provider/AuthProvider";
 
-
 const Bookings = () => {
     const { user } = useContext(AuthContext);
     const [booked, setBooked] = useState([]);
 
-    const url = `http://localhost:5000/booked?email=${user?.Email}`;
+    const handleDelete = id => {
+        const proceed = confirm('Are You Sure');
+        if (proceed) {
+            const deleteUrl = `http://localhost:5000/booked/${id}`;
+            fetch(deleteUrl, {
+                method: 'DELETE',
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.success) {
+                        // Refresh the data after successful deletion
+                        const url = `http://localhost:5000/booked?Email=${user?.email}`;
+                        fetch(url)
+                            .then(res => res.json())
+                            .then(data => setBooked(data))
+                            .catch(error => console.error('Error:', error));
+                    } else {
+                        console.error('Deletion failed:', data.message);
+                    }
+                })
+                .catch(error => console.error('Error:', error));
+        }
+    }
+
+    const url = `http://localhost:5000/booked?Email=${user?.email}`;
     useEffect(() => {
         fetch(url)
             .then(res => res.json())
             .then(data => setBooked(data))
+            .catch(error => console.error('Error:', error));
+    }, []);
 
-    }, [])
     return (
         <div>
-            <h1>My Booked Room :{booked.length} </h1>
+            <div className="overflow-x-auto p-10">
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Name</th>
+                            <th>Title</th>
+                            <th>Price</th>
+                            <th>Room Size</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {booked.map((row, index) => (
+                            <tr key={index}>
+                                <th>{index + 1}</th>
+                                <td>{row.Name}</td>
+                                <td>{row.Title}</td>
+                                <td>{row.Price}</td>
+                                <td>{row.Size}</td>
+                                <td>
+                                    <button onClick={() => handleDelete(row._id)} className="btn btn-outline btn-error">Delete</button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
